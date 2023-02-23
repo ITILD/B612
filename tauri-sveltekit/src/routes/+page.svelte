97a7362e310c0487cc7f4fea740a494d6e5e9f48
lpp <geolifestudy@gmail.com>
2023-02-23 22:41:40 +0800

    初始化svlltekit和tailwindcss

---
+++ tauri-sveltekit/src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+  import Greet from "$lib/Greet.svelte";
+</script>
+
+<h1>Welcome to Tauri!</h1>
+
+<div class="row">
+  <a href="https://vitejs.dev" target="_blank">
+    <img src="/vite.svg" class="logo vite" alt="Vite Logo" />
+  </a>
+  <a href="https://tauri.app" target="_blank">
+    <img src="/tauri.svg" class="logo tauri" alt="Tauri Logo" />
+  </a>
+  <a href="https://kit.svelte.dev" target="_blank">
+    <img src="/svelte.svg" class="logo svelte" alt="Svelte Logo" />
+  </a>
+</div>
+
+<p>Click on the Tauri, Vite, and Svelte logos to learn more.</p>
+
+<div class="row">
+  <Greet />
+</div>
+<h1 class="text-3xl font-bold underline">
+  Hello world!
+</h1>
+<h1 >
+  Hello world1!
+</h1>
+
+<style lang="postcss">
+    .logo.vite:hover {
+    filter: drop-shadow(0 0 2em #747bff);
+  }
+
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00);
+  }
+  
+  :global(html) {
+    background-color: theme(colors.red.100);
+  }
+
+
+</style>

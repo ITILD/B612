9a275d6f1e195706d5490d0e58b5520106191b06
lpp <geolifestudy@gmail.com>
2023-03-10 17:30:12 +0800

    实体

--- rust-server/main/src/zz1_template/common/test.rs
+++ rust-server/main/src/zz1_template/common/test.rs
@@ -1,12 +1,16 @@
 use sea_orm::{entity::{prelude::*}, Database};
 use super::super::entity_test::user;
-pub async fn test0() {
+pub async fn test0()-> Result<(), DbErr>{
     println!("test start!!!!!!!!!!");
     const DATABASE_URL: &str = "sqlite://db/test_tempalte.db";
     let db = Database::connect(DATABASE_URL)
         .await
         .expect("连接数据库失败");
     // Find by primary key
-    let cheese: Option<user::Model> = user::Entity::find_by_id(1).one(&db).await?;
-    println!("test end!!!!!!!!!!")
+    let user: Option<user::Model> = user::Entity::find_by_id(1).one(&db).await?;
+
+    println!("ID: {:?}", user);
+
+    Ok(())
+
 }

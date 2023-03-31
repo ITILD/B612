7ed45730dbb6f99544369ce3ba00ebbcd190c431
lpp <geolifestudy@gmail.com>
2023-03-31 17:48:03 +0800

    Create TODO.vue

---
+++ ts-web/vue/src/views/todo/TODO.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="test">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .test {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<!-- todo -->
+
+<!-- docxtemplater -->
+<!-- cesium -->
+<!-- 通用文件模板 -->
+<!-- babylon -->
+<!-- 描述模型 -->
+<!-- 室内设计 -->

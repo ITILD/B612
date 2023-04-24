bfd74b3656e9486f8048b549d0e1d7c3a1c70826
lpp <geolifestudy@gmail.com>
2023-04-24 17:30:34 +0800

    自动关闭弹窗组件

---
+++ ts-web/vue/src/components/common/minUi/animation/ShowHidden.vue
@@ -0,0 +1,8 @@
+<template>
+  <transition enter-active-class="duration-1000 ease-in" enter-from-class="transform opacity-0"
+    enter-to-class="opacity-100" leave-active-class="duration-1000 ease-in" leave-from-class="opacity-100"
+    leave-to-class="transform opacity-0">
+    <slot >
+    </slot>
+  </transition>
+</template>
---
+++ ts-web/vue/src/components/common/minUi/button/LppButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="changeButton" ref="dropdownRef" v-click-outside:[dropdownRef]="changeButton">
+    <slot> </slot>
+  </button>
+</template>
+<script setup lang="ts">
+import { ref, defineProps } from 'vue'
+/**
+ * 改变外部状态，点击外部自动关闭
+ */
+const props = defineProps({
+  modelValue: { type: Boolean, required: true }
+})
+const emit = defineEmits(['update:modelValue'])
+// 外部点击监听器  外部点击关闭
+const dropdownRef = ref<null | HTMLElement>(null)
+// 修改外部状态，点击内部自动关闭控件 （通过v-click-outside绑定到外部） （使用
+const changeButton = () => emit('update:modelValue', !props.modelValue)
+</script>

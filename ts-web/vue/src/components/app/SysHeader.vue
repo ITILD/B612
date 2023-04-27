2f5e0ac70cbfa0ed527b353530c40dd9ca139321
lpp <geolifestudy@gmail.com>
2023-04-27 17:32:25 +0800

    修改页脚

--- ts-web/vue/src/components/app/SysHeader.vue
+++ ts-web/vue/src/components/app/SysHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
     <div class="flex flex-wrap justify-between items-center mx-auto  p-4">
-      <a href="todo" class="flex items-center">
+      <router-link to="/" class="flex items-center">
         <img src="@/assets/star.svg" class="h-8 mr-3" alt="Flowbite Logo" />
         <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
           >TESTHOME</span
         >
-      </a>
+      </router-link>
       <!-- 小屏幕设备 下拉列表 -->
       <button
         id="mega-menu-full-image-button"
@@ -333,10 +333,9 @@
             >
           </li>
           <li>
-            <a
-              href="#"
+            <router-link to="/_dev"
               class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700"
-              >_dev</a
+              >_dev</router-link
             >
           </li>
         </ul>
--- ts-web/vue/src/router/index.ts
+++ ts-web/vue/src/router/index.ts
@@ -4,7 +4,12 @@ import MainPage from '../views/index-page.vue'
 // const modules = AutoViewAndLink.routes()
 const routes = [
   { path: '/', component: MainPage },
-  { path: '/dev', component: () => import('../views/_dev/index-page.vue') },
+  // 开发测试
+  { path: '/_dev', component: () => import('../views/_dev/index-page.vue') },
+  { path: '/_dev/map2d', component: () => import('../views/_dev/map/map2d-page.vue') },
+  { path: '/_dev/map3d', component: () => import('../views/_dev/map/map3d-page.vue') },
+  { path: '/_dev/map-home', component: () => import('../views/_dev/map/map-home-page.vue') },
+
   // { path: '/info', name: 'home1', component: () => import('../views/info/list-i-p.vue') },
   // {
   //   path: '/user',

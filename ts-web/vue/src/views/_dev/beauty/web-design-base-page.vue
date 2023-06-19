176b45bb329dd4297075037f085d4cf5b2a1a880
lpp <geolifestudy@gmail.com>
2023-06-19 17:17:47 +0800

    页面设计

--- ts-web/vue/src/views/_dev/beauty/web-design-base-page.vue
+++ ts-web/vue/src/views/_dev/beauty/web-design-base-page.vue
@@ -1,76 +1,148 @@
 <template>
-  <div class="this-page">
-    <!-- 地图-->
-    <canvas id="glDom" class="glDom"></canvas>
+  <div class="">
+    <!-- 基础页面纸张 -->
+    <div class="box1 grid grid-cols-12">
+      <div class="layer1 h-52">
+        <div class="layer1 h-full"></div>
+      </div>
+      <div class="h-52">
+        <div class="layer2 h-full"></div>
+      </div>
+      <div class="layer1 layer2 h-52"></div>
+    </div>
+    <div class="box2 grid grid-cols-12">
+      <div class="layer1 h-52"></div>
+      <div class="layer2 h-52"></div>
+      <div class="layer1 layer2 h-52"></div>
+    </div>
+
+    <ul class="_1by9knb3">
+      <li class="_1by9knb4">
+       
+        <div class="_1by9knb5"><div class="layer1"></div></div>
+        <p>
+          <span
+            _msttexthash="83005"
+            _msthash="347"
+            >.layer1</span
+          >
+        </p>
+      </li>
+      <li class="_1by9knb4">
+        <p>
+          <span class="cjm7lg9 cjm7lg0 _1qru72h0 __className_0db19d"
+            ><span class="_1i0yhbt0" _msttexthash="57004181" _msthash="348"
+              >.layer2（无混合模式）</span
+            ></span
+          >
+        </p>
+        <div class="_1by9knb5"><div class="layer2"></div></div>
+      </li>
+      <li class="_1by9knb4">
+        <p>
+          <span class="cjm7lg9 cjm7lg0 _1qru72h0 __className_0db19d"
+            ><span class="_1i0yhbt0">.layer2（带混合模式</span></span
+          >
+        </p>
+        <div class="_1by9knb5">
+          <div class="layer1"></div>
+          <div class="layer2 blendLayer2"></div>
+        </div>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, onBeforeUnmount } from 'vue'
-// import {Engine,Scene,ArcRotateCamera,Vector3,HemisphericLight,PointLight,MeshBuilder} from 'babylonjs';
-import * as BABYLON from '@babylonjs/core/Legacy/legacy'
-let scene: BABYLON.Scene
-let engine: BABYLON.Engine
-let camera: BABYLON.FreeCamera
 // vue
-onMounted(() => {
-  initMap()
-  window.$ObjLargeTemp.set('scene', scene)
-})
-onBeforeUnmount(() => {
-  scene && scene.dispose()
-  window.$ObjLargeTemp.delete('scene')
-})
-const initMap = () => {
-  // Get the canvas DOM element
-  const canvas = document.getElementById('glDom') as HTMLCanvasElement // 得到canvas对象的引用
-  // Load the 3D engine // 初始化 BABYLON 3D engine
-  engine = new BABYLON.Engine(canvas, true, {
-    preserveDrawingBuffer: true,
-    stencil: true
-  })
-  /******* CreateScene function that creates and return the scene ******/
-
-  // Create a basic BJS Scene object 创建一个场景scene
-  scene = new BABYLON.Scene(engine)
-  // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}  添加一个相机，并绑定鼠标事件
-  camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene)
-  // Target the camera to scene origin
-  camera.setTarget(BABYLON.Vector3.Zero())
-  // Attach the camera to the canvas
-  camera.attachControl(canvas, false)
-  // Create a basic light, aiming 0, 1, 0 - meaning, to the sky 添加一组灯光到场景
-  const light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene)
-  const light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene)
-  //添加一个球体到场景中
-  // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-  const sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE)
-  // Move the sphere upward 1/2 of its height
-  sphere.position.y = 1
-  // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-  const ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false)
-  // Return the created scene
-  // run the render loop 最后一步调用engine的runRenderLoop方案，执行scene.render()，让我们的3d场景渲染起来
-  engine.runRenderLoop(function () {
-    scene.render()
-  })
-  // the canvas/window resize event handler 监听浏览器改变大小的事件，通过调用engine.resize()来自适应窗口大小
-  window.addEventListener('resize', function () {
-    engine.resize()
-  })
-}
+onMounted(() => {})
+onBeforeUnmount(() => {})
 </script>
 
-<style scoped>
-.this-page {
-  max-height:100% ;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+<style scoped lang="scss">
+.box1 {
+  .layer1 {
+    background: hsl(40, 35%, 76%);
+  }
+
+  .layer2 {
+    box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
+    background: url('@/assets/lib/_dev/beauty/noise.webp'),
+      linear-gradient(to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1));
+    mix-blend-mode: overlay;
+  }
 }
-.glDom {
+
+.box2 {
+  .layer1 {
+    background: hsl(40, 35%, 76%);
+  }
+
+  .layer2 {
+    box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
+    background: url('@/assets/lib/_dev/beauty/waves.webp'),
+      linear-gradient(to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1));
+    mix-blend-mode: overlay;
+  }
+}
+
+._1by9knb3 {
+  display: grid;
+  grid-gap: var(--_1by9knb0);
+  grid-template-columns: repeat(1, 1fr);
+
+  ._1by9knb4 {
+    width: 100%;
+    min-width: 0;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: var(--_1by9knb0);
+
+  }
+  
+  ._1by9knb5 {
+      width: 100%;
+      aspect-ratio: 1;
+      position: relative;
+      overflow: hidden;
+      border: var(--_1xn0dsy1x);
+    }
+    .layer1 {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background: hsl(40, 35%, 76%);
+    }
+    .blendLayer2 {
+    mix-blend-mode: overlay;
+}
+
+    ._1i0yhbt0 {
+      display: block;
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+
+     .layer2 {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
+    background: url('@/assets/lib/_dev/beauty/noise.webp'), linear-gradient( to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1) );
+    // background: url(https://garden.bradwoods.io/images/noise.webp), linear-gradient( to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1) );
+}
+}
+._1by9knb1 {
+  --_1by9knb0: 8px;
   width: 100%;
-  height:100%;
-  overflow: hidden;
+  position: relative;
+
 }
 </style>

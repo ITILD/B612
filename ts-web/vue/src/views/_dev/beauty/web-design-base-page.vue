a54dbd7795e52d3e7f7b51ac0f2b257cdfb7e802
lpp <geolifestudy@gmail.com>
2023-06-26 13:54:47 +0800

    test

--- ts-web/vue/src/views/_dev/beauty/web-design-base-page.vue
+++ ts-web/vue/src/views/_dev/beauty/web-design-base-page.vue
@@ -1,22 +1,8 @@
 <template>
   <div class="">
     <!-- 基础页面纸张 -->
-    <div class="box1 grid grid-cols-12">
-      <div class="layer1 h-52">
-        <div class="layer1 h-full"></div>
-      </div>
-      <div class="h-52">
-        <div class="layer2 h-full"></div>
-      </div>
-      <div class="layer1 layer2 h-52"></div>
-    </div>
-    <div class="box2 grid grid-cols-12">
-      <div class="layer1 h-52"></div>
-      <div class="layer2 h-52"></div>
-      <div class="layer1 layer2 h-52"></div>
-    </div>
-
-    <ul class="box3">
+    <!-- 混合仿纸质 1-->
+    <ul class="box1">
       <li>
         <div class="layerBox"><div class="layer1"></div></div>
         <p>
@@ -39,6 +25,32 @@
         </p>
       </li>
     </ul>
+    <!--  -->
+    <!--混合仿纸质 2-->
+    <ul class="box2">
+      <li>
+        <div class="layerBox"><div class="layer1"></div></div>
+        <p>
+          <span>.layer1</span>
+        </p>
+      </li>
+      <li>
+        <div class="layerBox"><div class="layer2"></div></div>
+        <p>
+          <span>.layer2（无混合模式）</span>
+        </p>
+      </li>
+      <li>
+        <div class="layerBox">
+          <div class="layer1"></div>
+          <div class="layer2 blendLayer2"></div>
+        </div>
+        <p>
+          <span>.layer2（混合 仿纸质）</span>
+        </p>
+      </li>
+    </ul>
+    <!--  -->
   </div>
 </template>
 
@@ -50,33 +62,8 @@ onBeforeUnmount(() => {})
 </script>
 
 <style scoped lang="scss">
+
 .box1 {
-  .layer1 {
-    background: hsl(40, 35%, 76%);
-  }
-
-  .layer2 {
-    box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
-    background: url('@/assets/lib/_dev/beauty/noise.webp'),
-      linear-gradient(to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1));
-    mix-blend-mode: overlay;
-  }
-}
-
-.box2 {
-  .layer1 {
-    background: hsl(40, 35%, 76%);
-  }
-
-  .layer2 {
-    box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
-    background: url('@/assets/lib/_dev/beauty/waves.webp'),
-      linear-gradient(to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1));
-    mix-blend-mode: overlay;
-  }
-}
-
-.box3 {
   display: grid;
   grid-gap: var(--_1by9knb0);
   grid-template-columns: repeat(12, 1fr);
@@ -109,4 +96,63 @@ onBeforeUnmount(() => {})
     mix-blend-mode: overlay;
   }
 }
+// 画混合
+
+
+.box2 {
+  display: grid;
+  grid-gap: var(--_1by9knb0);
+  grid-template-columns: repeat(12, 1fr);
+
+  .layerBox {
+    width: 100%;
+    aspect-ratio: 9/16;
+    position: relative;
+    overflow: hidden;
+    border: var(--_1xn0dsy1x);
+  }
+  .layer1 {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: hsl(40, 35%, 76%);
+  }
+
+  .layer2 {
+    width: 100%;
+    height: 100%;
+    box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
+    background: url('@/assets/lib/_dev/beauty/waves.webp'),
+      linear-gradient(to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1));
+  }
+  // 混合方式
+  .blendLayer2 {
+    mix-blend-mode: overlay;
+  }
+}
+
+
+
+
+
+
+
+
+
+// .box2 {
+  
+//   .layer1 {
+//     background: hsl(40, 35%, 76%);
+//   }
+
+//   .layer2 {
+//     box-shadow: inset 0 0 40px 10px hsla(0, 0%, 0%, 0.8);
+//     background: url('@/assets/lib/_dev/beauty/waves.webp'),
+//       linear-gradient(to bottom right, hsla(0, 0%, 0%, 0) 40%, hsla(0, 0%, 0%, 1));
+//     mix-blend-mode: overlay;
+//   }
+// }
+
 </style>
